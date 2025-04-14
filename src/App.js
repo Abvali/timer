a2ac9b94b85184b4edaff8a0521d3eedc3554153
Maryam <abvali.maryam@gmail.com>
2025-04-14 14:36:07 +0200

    Timer starting

--- src/App.js
+++ src/App.js
@@ -1,11 +1,52 @@
-import './App.css';
+import React, { useEffect, useState } from "react";
+import "./App.css";
 
-function App() {
+export default function Timer() {
+  const [time, setTime] = useState(3661);
+  const [isRunning, setIsRunning] = useState(false);
+
+  function formatTime() {
+    const hour = Math.floor(time / 3600);
+    const minute = Math.floor((time % 3600) / 60);
+    const second = time % 60;
+    return `${String(hour).padStart(2, "0")}:${String(minute).padStart(
+      2,
+      "0"
+    )}:${String(second).padStart(2, "0")}`;
+  }
+
+  useEffect(() => {
+    let interval;
+    if (isRunning) {
+      interval = setInterval(() => {
+        setTime((preveTime) => (preveTime > 0 ? preveTime - 1 : 0));
+      }, 1000);
+      
+    }
+    return()=> clearInterval(interval)
+  }, [isRunning]);
+
+  function handleStart() {
+    setIsRunning(true);
+  }
+  function handlePause() {setIsRunning(false)}
+  function handleReset() {setTime(3661)
+    setIsRunning(false)
+  }
   return (
-    <div>
-
+    <div className="timer-container">
+      <h1 className="timer-display">{formatTime(time)}</h1>
+      <div className="timer-buttons">
+        <button className="start-button" onClick={handleStart}>
+          Start
+        </button>
+        <button className="pause-button" onClick={handlePause}>
+          Pause
+        </button>
+        <button className="reset-button" onClick={handleReset}>
+          Reset
+        </button>
+      </div>
     </div>
   );
 }
-
-export default App;
